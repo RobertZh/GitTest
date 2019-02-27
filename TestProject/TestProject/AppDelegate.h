ecd33629d1a6afb27c128878ab8817dd8821c245
zhangwei <zhangwei8256@icloud.com>
2019-02-27 17:17:40 +0800

    Create Project.

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by zhangwei on 2019/2/27.
+//  Copyright © 2019 zhangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by zhangwei on 2019/2/27.
+//  Copyright © 2019 zhangwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
